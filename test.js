063119cb7b022af9bc798478a3f502bd7481a6f7
Tibor Krenacs <krenacs.tibor@gmail.com>
2019-07-18 10:20:27 +0200

    last

--- test.js
+++ test.js
@@ -71,15 +71,14 @@ console.log(metszet([1, 2, 3, 4, 5], [5, 6, 2, 7, 1]));
 const add = (a, b) => {
   return a + b;
 };
-const sub = (a, b) => {
-  return a - b;
-};
+
 const mul = (a, b) => {
   return a * b;
 };
-const div = (a, b) => {
-  return a / b;
+const hatv = (a, b) => {
+  return a ^ b ;
 };
+
 const operation = (a, b, o) => {
   return o(a, b);
 };
